cbb55fb89f2aed03583196ec8794e3c4316e611e
dramo3 <bu.ph@web.de>
2023-01-17 14:20:01 +0100

    Second Commit

--- sketch.js
+++ sketch.js
@@ -7,8 +7,12 @@
 // p5.js Web Editor - Webcam: https://editor.p5js.org/codingtrain/sketches/VIYRpcME3
 // p5.js Web Editor - Webcam Persistence: https://editor.p5js.org/codingtrain/sketches/Vt9xeTxWJ
 
-// let img;
+let emoPos1;
+let emoPos2;
+let emoNeg1;
+let emoNeg2;
 let video;
+let feed;
 let detector;
 let detections = {};
 let idCount = 0;
@@ -16,8 +20,13 @@ let title = [0, 0, 0];
 
 function preload() {
   // img = loadImage('dog_cat.jpg');
+  emoPos1 = loadImage('emoPos1.png');
+  emoPos2 = loadImage('emoPos2.png');
+  emoNeg1 = loadImage('emoNeg1.png');
+  emoNeg2 = loadImage('emoNeg2.png');
   detector = ml5.objectDetector('cocossd');
 }
+
 
 function gotDetections(error, results) {
   if (error) {
@@ -80,35 +89,44 @@ function gotDetections(error, results) {
   detector.detect(video, gotDetections);
 }
 
-function setup() {
-  createCanvas(640 + 540, 480);
-  video = createCapture(VIDEO);
-  video.size(640, 480);
-  video.hide();
-  detector.detect(video, gotDetections);
-  //vid = createVideo(
-  //  ['FB_Feed_v1_1.mp4', 'assets/small.ogv', 'assets/small.webm'],
-   // vidLoad);
-  
-  feed = createVideo(
-    ['IG_Feed_v1_2.mp4', 'assets/small.ogv', 'assets/small.webm'],
-    vidLoad
-  );
 
-  feed.size(540, 480);
-  feed.position(540, 0);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(185);
+  video = createCapture(VIDEO);
+  video.size(960, 720);
+  video.hide();
+  detector.detect(video, gotDetections); 
+  feed = createVideo(
+    ['IG_Feed_v1_2.mp4', 'assets/small.ogv', 'assets/small.webm'], vidLoad);
+  feed.size(1442, AUTO);
+  //feed.position(960 + windowWidth/8, 390 + windowHeight/8);
 }
   
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  // video.size(windowWidth - 200, windowHeight - 200);
+}
+
+
+function mousePressed() {
+	let fs = fullscreen();
+	fullscreen(!fs);
+}
 
 
 function vidLoad() {
   feed.loop();
   feed.volume(0);
+  //feed.position(960 + windowWidth/8, 390 + windowHeight/8);
 }
 
 
 function draw() {
-  image(video, 0, 0);
+  image(video, windowWidth/8, windowHeight/8, 960, 720);
+  //video.size(windowWidth - 200, windowHeight - 200);
+  feed.position(510 + windowWidth/8, windowHeight/8);
 
   
   let labels = Object.keys(detections);
@@ -117,29 +135,55 @@ function draw() {
     for (let i = objects.length - 1; i >= 0; i--) {
       let object = objects[i];
       if (object.label == 'person') {
-        if (object.x < 320) {
+        if (object.x <= 240) {
         //fill('rgba(0,255,0, 0)');
+        image(emoPos2, object.x - 45, object.y + 35 - 30);
         noFill();
-        stroke(255, 0, 0);
+        stroke(85);
         strokeWeight(3);
         rect(object.x, object.y, object.width, object.height);
-        noStroke();
-        fill(0, 0, 0);
+        //noStroke();
+        //fill(0, 0, 0);
         //strokeWeight(3);
-        text(object.label + " Opposing " + object.id, object.x + 10, object.y + 24);
-        textSize(28);
+        //text(object.label + " No. " + object.id, object.x + 10, object.y + 30);
+        //textSize(28);
         }
-        if (object.x > 320) {
+        if (object.x > 240 && object.x <= 480) {
          //fill(0, 255, 0, object.timer);
+        image(emoPos1, object.x + 5, object.y + 35 -30);
         noFill();
-        stroke(0, 255, 0);
+        stroke(85);
         strokeWeight(3);
         rect(object.x, object.y, object.width, object.height);
-        noStroke();
-        fill(0, 0, 0);
-        text(object.label + " Favouring " + object.id, object.x + 10, object.y + 24);
-        textSize(28);
+        //noStroke();
+        //fill(0, 0, 0);
+        //text(object.label + " No. " + object.id, object.x + 10, object.y + 30);
+        //textSize(28);
       }
+      if (object.x > 480 && object.x <= 720) {
+        //fill(0, 255, 0, object.timer);
+       image(emoNeg1, object.x +10, object.y + 35 - 30);
+       noFill();
+       stroke(85);
+       strokeWeight(3);
+       rect(object.x, object.y, object.width, object.height);
+       //noStroke();
+       //fill(0, 0, 0);
+       //text(object.label + " No. " + object.id, object.x + 10, object.y + 24);
+       //textSize(28);
+     }
+     if (object.x > 720 && object.x <= 960) {
+      //fill(0, 255, 0, object.timer);
+     image(emoNeg2, object.x +10, object.y + 35 - 30);
+     noFill();
+     stroke(85);
+     strokeWeight(3);
+     rect(object.x, object.y, object.width, object.height);
+     //noStroke();
+     //fill(0, 0, 0);
+     //text(object.label + " No. " + object.id, object.x + 10, object.y + 24);
+     //textSize(28);
+   }
       }
       object.timer -= 2;
       if (object.timer < 0) {
